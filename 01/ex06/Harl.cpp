6a40a5004feb71c2c87264cb56c8a83796e6ee96
Bobbie Syväsalmi <bsyvasal@student.hive.fi>
2024-03-12 14:35:32 +0200

    updated

--- 01/ex06/Harl.cpp
+++ 01/ex06/Harl.cpp
@@ -4,18 +4,21 @@ void Harl::debug( void ) {
 	std::cout << "[DEBUG]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup"
 		" burger. I really do!" << std::endl;
+	Harl::info();
 }
 
 void Harl::info( void ) {
 	std::cout << "[INFO]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put"
 		"enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	Harl::warning();
 }
 
 void Harl::warning( void ) {
 	std::cout << "[WARNING]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for"
 		"years whereas you started working here since last month." << std::endl;
+	Harl::error();
 }
 
 void Harl::error( void ) {
@@ -32,25 +35,14 @@ void Harl::complain( std::string level ) {
 		i++;
 	switch (i)
 	{
-		case 0:
-			(this->*func[0])();
-			(this->*func[1])();
-			(this->*func[2])();
-			(this->*func[3])();
+		case 0: (this->*func[0])();
 			break;
-		case 1:
-			(this->*func[1])();
-			(this->*func[2])();
-			(this->*func[3])();
+		case 1: (this->*func[1])();
 			break;
-		case 2:
-			(this->*func[2])();
-			(this->*func[3])();
+		case 2: (this->*func[2])();
 			break;
-		case 3: 
-			(this->*func[3])();
+		case 3: (this->*func[3])();
 			break;
-		default:
-			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+		default: std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 	}
 }
