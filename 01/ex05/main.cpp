cdf9c5b1ee8f5994bf9899c0c9f78b151c6cd4a0
Bobbie Syväsalmi <bsyvasal@student.hive.fi>
2024-03-12 14:40:35 +0200

    added

---
+++ 01/ex05/main.cpp
@@ -0,0 +1,16 @@
+#include "Harl.hpp"
+
+int main(void)
+{
+  Harl harl;
+
+  harl.complain("DEBUG");
+  harl.complain("INFO");
+  harl.complain("WARNING");
+  harl.complain("ERROR");
+  harl.complain("NOTHING");
+  harl.complain("MKEH");
+  harl.complain("COMPLAIN");
+
+  return (0);
+}
