0698cc292a742ed0381d93abd8fe83cf82586a07
Bobbie Syväsalmi <bsyvasal@student.hive.fi>
2024-03-14 17:53:17 +0200

    updated

--- 02/ex02/main.cpp
+++ 02/ex02/main.cpp
@@ -2,11 +2,17 @@
 
 int main( void ) {
 Fixed a;
-Fixed const b( 10 );
+Fixed const b( 10000 );
 Fixed const c( 42.42f );
 Fixed const d( b );
 a = Fixed( 1234.4321f );
+
 std::cout << "a is " << a << std::endl;
+std::cout << "b is " << b << std::endl;
+if (a > b)
+	std::cout << "a is bigger than b: " << std::endl;
+else
+	std::cout << "b is bigger than a" << std::endl;
 
 return 0;
 }
