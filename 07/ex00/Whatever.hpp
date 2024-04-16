f4ab35f514e76fa5cf7d928a320bdd816f913672
Bobbie Syväsalmi <bsyvasalmi@student.hive.fi>
2024-04-17 01:17:04 +0300

    updated

--- 07/ex00/Whatever.hpp
+++ 07/ex00/Whatever.hpp
@@ -1,25 +1,21 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
-class Whatever
+template <typename T> static void swap(T &a, T &b)
 {
-	public:
-	template <typename T> static void swap(T &a, T &b)
-	{
-		T tmp = a;
-		a = b;
-		b = tmp;
-	}
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
 
-	template <typename T> static T min(T a, T b)
-	{
-		return (a < b) ? a : b;
-	}
+template <typename T> static T min(T a, T b)
+{
+	return (a < b) ? a : b;
+}
 
-	template <typename T> static T max(T a, T b)
-	{
-		return (a > b) ? a : b;
-	}
-};
+template <typename T> static T max(T a, T b)
+{
+	return (a > b) ? a : b;
+}
 
 #endif
