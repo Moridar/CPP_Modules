2c8827dccc5a55b15058a955d326b0e36f9a00c2
Bobbie Syväsalmi <bsyvasalmi@student.hive.fi>
2024-04-17 01:32:34 +0300

    added

---
+++ 07/ex01/main.cpp
@@ -0,0 +1,25 @@
+#include "iter.hpp"
+#include <iostream>
+
+void print(int &i)
+{
+	std::cout << i << std::endl;
+}
+
+void addone(int &i)
+{
+	i += 1;
+}
+
+int main()
+{
+	int arr[] = {1, 2, 3, 4, 5};
+	int len = sizeof(arr) / sizeof(arr[0]);
+
+	::iter(arr, len, print);
+	::iter(arr, len, addone);
+	std::cout << "==After adding one to each element==" << std::endl;
+	::iter(arr, len, print);
+
+	return 0;
+}
