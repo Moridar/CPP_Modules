2f677898e8916ce1859f09add1f3eca94f01cf6d
Bobbie Syväsalmi <bsyvasalmi@student.hive.fi>
2024-05-29 10:18:20 +0300

    updated

--- 06/ex02/Base.cpp
+++ 06/ex02/Base.cpp
@@ -10,28 +10,16 @@ Base::~Base()
 
 Base *Base::generate(void)
 {
-	Base *base = NULL;
+	Base *base;
 	srand(time(NULL));
 	int random = rand() % 3;
-	if (random == 0)
-		base = new A();
-	else if (random == 1)
-		base = new B();
-	else
-		base = new C();
+	random == 0 ? base = new A() : random == 1 ? base = new B() : base = new C();
 	return (base);
 }
 
 void Base::identify(Base *p)
 {
-	if (dynamic_cast<A *>(p))
-		std::cout << "A" << std::endl;
-	else if (dynamic_cast<B *>(p))
-		std::cout << "B" << std::endl;
-	else if (dynamic_cast<C *>(p))
-		std::cout << "C" << std::endl;
-	else
-		std::cout << "Unknown" << std::endl;
+	std::cout << (dynamic_cast<A *>(p) ? "A" : dynamic_cast<B *>(p) ? "B" : dynamic_cast<C *>(p) ? "C" : "Unknown") << std::endl;
 }
 
 void Base::identify(Base &p)
