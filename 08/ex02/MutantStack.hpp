486ccec0f1af9447e5a8c850fd978acf4e6a978a
Bobbie Syväsalmi <bsyvasalmi@student.hive.fi>
2024-06-01 23:21:53 +0300

    updated

--- 08/ex02/MutantStack.hpp
+++ 08/ex02/MutantStack.hpp
@@ -4,15 +4,36 @@
 #include <stack>
 #include <algorithm>
 
-template<class T>
+template<typename T>
 class MutantStack : public std::stack<T>
 {
 	public:
+		MutantStack() : std::stack<T>() {}
+		~MutantStack() {}
+		MutantStack(const MutantStack &copy) : std::stack<T>(copy) {}
+		MutantStack &operator=(const MutantStack &assign)
+		{
+			std::stack<T>::operator=(assign);
+			return *this;
+		}
+		
+		template <typename InputIterator>
+		MutantStack(InputIterator begin, InputIterator end) : std::stack<T>() 
+		{
+			while (&*begin < &*end)
+			{
+				this->push(*begin);
+				begin++;
+			}
+		}
+		
 		typedef typename std::stack<T>::container_type::iterator iterator;
+		
 		iterator begin()
 		{
 			return std::stack<T>::c.begin();
 		}
+		
 		iterator end() 
 		{
 			return std::stack<T>::c.end();
--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -1,13 +1,16 @@
 #include "MutantStack.hpp"
 #include <iostream>
 #include <queue>
+#include <list>
 
 int main()
 {
+	std::cout << "==1. Create MutantStack, push 5 and 7, shows the top==" << std::endl;
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
 	std::cout << mstack.top() << std::endl;
+	std::cout << "==2. Pop the top, show the size, push 3, 5, 737, 0==" << std::endl;
 	mstack.pop();
 	std::cout << mstack.size() << std::endl;
 	mstack.push(3);
@@ -19,11 +22,33 @@ int main()
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
 	--it;
+	std::cout << "==3. Iterate through the stack and print the values==" << std::endl;
 	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << *it << std::endl;
+		++it;
 	}
-	std::stack<int> s(mstack);
+	std::cout << "==4. Create a list from the stack and print the values==" << std::endl;
+	std::list<int> mylist(mstack.begin(), mstack.end());
+	for (int i : mylist)
+		std::cout << i << std::endl;
+	
+	std::cout << "==5. Create a new stack from the list and push 42 and print the values==" << std::endl;
+	MutantStack<int> *newStack = new MutantStack<int>(mylist.begin(), mylist.end());
+	newStack->push(42);
+	for (int i : *newStack)
+		std::cout << i << std::endl;
+
+	std::cout << "==6. Copies the last stack, deletes it and print the values==" << std::endl;
+	MutantStack<int> copyStack(*newStack);
+	delete newStack;
+	for (int i : copyStack)
+		std::cout << i << std::endl;
+
+	std::cout << "==7. Assign a new stack with the copy stack and print the values==" << std::endl;
+	MutantStack<int> assignStack = copyStack;
+	copyStack.push(43);
+	for (int i : assignStack)
+		std::cout << i << std::endl;
 	return 0;
 }
