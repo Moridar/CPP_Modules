15650d7f4b2dd1a4141ea6096234c0a7e9b783c6
Bobbie Syvasalmi <bsyvasal@c1r4p5.hive.fi>
2024-06-03 14:55:04 +0300

    updated

--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -1,25 +1,42 @@
 #include "iter.hpp"
 #include <iostream>
 
-void print(int &i)
+template <typename T>
+static void print(T &i)
 {
 	std::cout << i << std::endl;
 }
 
-void addone(int &i)
+template <typename T>
+static void addone(T &i)
 {
 	i += 1;
 }
 
 int main()
 {
+	std::cout << "==1. Creates an array {1, 2, 3, 4, 5}==" << std::endl;
 	int arr[] = {1, 2, 3, 4, 5};
 	int len = sizeof(arr) / sizeof(arr[0]);
 
+	std::cout << len << std::endl;
+	std::cout << "==2. Iter and print every element==" << std::endl;
 	iter(arr, len, print);
+	std::cout << "==3. Iter and add one to every element==" << std::endl;
 	iter(arr, len, addone);
-	std::cout << "==After adding one to each element==" << std::endl;
+	std::cout << "==4. Iter and print every element==" << std::endl;
 	iter(arr, len, print);
+
+	std::cout << "==5. Creates a new array {a, b, c, d, e} and print every element==" << std::endl;
+	char arr1[] = {'a', 'b', 'c', 'd', 'e'};
+	len = sizeof(arr) / sizeof(arr[0]);
+	iter(arr1, len, print);
+	std::cout << "==6. Iter and add one to every element==" << std::endl;
+	iter(arr1, len, addone);
+	std::cout << "==7. Iter and print every element==" << std::endl;
+	iter(arr1, len, print);
+
+
 
 	return 0;
 }
