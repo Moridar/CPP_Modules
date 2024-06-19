b539831b79a881af60a950fdf7a9c71d0c78d10b
Bobbie Syväsalmi <bsyvasal@student.hive.fi>
2024-06-19 16:47:28 +0300

    bug on last element

--- 09/ex02/main.cpp
+++ 09/ex02/main.cpp
@@ -1,8 +1,8 @@
-#include "PmergeME.hpp"
+#include "PmergeMe.hpp"
 
 //to test multiple number: ./PmergeMe `jot -r 3000 1 100000 | tr '\n' ' '`
 
 int main(int argc, char **argv)
 {
-	PmergeME::pmerge(argc, argv);
+	PmergeMe::pmerge(argc, argv);
 }
