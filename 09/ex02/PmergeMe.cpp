a9b32d637feb70b026718efd745713a89f08beab
Bobbie Syvasalmi <bsyvasal@student.hive.fi>
2024-06-20 09:58:03 +0300

    finished

--- 09/ex02/PmergeMe.cpp
+++ 09/ex02/PmergeMe.cpp
@@ -10,14 +10,14 @@ void PmergeMe::pmerge(int argc, char **argv)
 	try
 	{
 		me.fill_containers(argc, argv);
-		me.print("Before: ", me._vec);
+		me.print("Before      : ", me._vec);
 		auto vecstart = std::chrono::high_resolution_clock::now();
-		me._vec = me.pmerge_vec();
+		me.pmerge_vec();
 		auto vecend = std::chrono::high_resolution_clock::now();
-		auto lstart = std::chrono::high_resolution_clock::now();
-		me._list = me.pmerge_list();
-		auto lend = std::chrono::high_resolution_clock::now();
 		me.print("After vector: ", me._vec);
+		auto lstart = std::chrono::high_resolution_clock::now();
+		me.pmerge_list();
+		auto lend = std::chrono::high_resolution_clock::now();
 		me.print("After list  : ", me._list);
 		double vdur = std::chrono::duration_cast<std::chrono::nanoseconds>(vecend - vecstart).count();
 		double ldur = std::chrono::duration_cast<std::chrono::nanoseconds>(lend - lstart).count();
@@ -75,47 +75,82 @@ static int get_next_jacobsthal(int n, int &jacob_seq, size_t size)
 	return (std::min(get_jacobsthal(++jacob_seq) - 1, (int) size - 1));
 }
 
-//4. Insert at the start of S, the elemented paired with smallest element in S.
-//5. Insert the remaining, following Jacobsthal sequence.
-t_intvec PmergeMe::pmerge_vec()
+//1. MergeSort:
+//1.1 Splits the input elements until 2
+//1.2 Create pair with larger on first.
+//1.3 Recursively sort
+//4. Insertion: 
+//4.1 Insert at the start of S, the elemented paired with smallest element in S.
+//4.2 Insert the remaining, following Jacobsthal sequence.
+void PmergeMe::pmerge_vec()
 {
-	t_pairvec pair_vec;
+	t_pair odd;
 	if (_vec.size() <= 1)
-		return (_vec);
+		return ;
 	
-	//1. Creates Pair
-	while (_vec.size() > 1)
-	{
-		pair_vec.push_back(std::make_pair(_vec[0], _vec[1]));
-		_vec.erase(_vec.begin(), _vec.begin() + 2);
-	}
-	
-	//2. Determines larger in pair, sets it to first. 
-	for (t_pair &p : pair_vec)
-		if (p.second > p.first)
-			std::swap(p.first, p.second);
-	
-	//3. Sorts pair_vec
-	std::sort(pair_vec.begin(), pair_vec.end());
-	
-	//3.1 Creates a sorted sequence S of the larger numbers of each pair.
-	t_intvec vec;
-	for (auto &p : pair_vec)
-		vec.push_back(p.first);
-	
-	insertion(pair_vec, vec);
-	//6. If the orignal vector is odd, insert the last element.
 	if (_vec.size() % 2 == 1)
 	{
-		t_veciterator it = vec_binary_search(vec, 0, vec.size() - 1, _vec.back());
-		vec.insert(it, _vec.back());
+		odd = std::make_pair(1, _vec.back());
+		_vec.pop_back();
 	}
-	return (vec);
+	t_pairvec pair_vec = mergeSort(0, _vec.size() - 1);
+	//3.1 Creates a sorted sequence S of the larger numbers of each pair.
+	_vec.clear();
+	for (t_pair &p : pair_vec)
+		_vec.push_back(p.first);
+	
+	insertion(pair_vec);
+	//6. If the orignal vector is odd, insert the last element.
+	if (odd.first)
+	{
+		t_veciterator it = vec_binary_search(0, _vec.size() - 1, odd.second);
+		_vec.insert(it, odd.second);
+	}
 }
 
-void PmergeMe::insertion(t_pairvec &pair_vec, t_intvec &vec)
+t_pairvec PmergeMe::mergeSort(int left, int right)
 {
-	vec.insert(vec.begin(), pair_vec.begin()->second);
+	int size = right - left + 1;
+	if (size <= 2)
+	{
+		t_pairvec pairvec;
+		t_pair pair;
+		pair = _vec[left] > _vec[left + 1] ? std::make_pair(_vec[left], _vec[left + 1]) : std::make_pair(_vec[left + 1], _vec[left]);
+		pairvec.push_back(pair);
+		return pairvec;
+	}
+
+	int m = left + size / 2 - 1;
+	if (m % 2 == 0)
+		m++;
+	t_pairvec leftvec = mergeSort(left, m);
+	t_pairvec rightvec = mergeSort(m + 1, right);
+	return merge(leftvec, rightvec);
+}
+
+t_pairvec PmergeMe::merge(t_pairvec left, t_pairvec right)
+{
+	t_pairvec pairvec;
+	size_t i = 0;
+	size_t j = 0;
+
+	while (i < left.size() && j < right.size())
+	{
+		if (left[i] > right[j])
+			pairvec.push_back(right[j++]);
+		else
+			pairvec.push_back(left[i++]);
+	}
+	while (i < left.size())
+		pairvec.push_back(left[i++]);
+	while (j < right.size())
+		pairvec.push_back(right[j++]);
+	return (pairvec);
+}
+
+void PmergeMe::insertion(t_pairvec &pair_vec)
+{
+	_vec.insert(_vec.begin(), pair_vec.begin()->second);
 	
 	if (pair_vec.size() != 1)
 	{
@@ -123,23 +158,23 @@ void PmergeMe::insertion(t_pairvec &pair_vec, t_intvec &vec)
 		int inserted = 1;
 		for (size_t i = std::min((int) pair_vec.size() - 1, 2); i < pair_vec.size(); i = get_next_jacobsthal(i, jacob_seq, pair_vec.size()))
 		{
-			t_veciterator it = vec_binary_search(vec, 0, i + inserted, pair_vec[i].second);
-			vec.insert(it, pair_vec[i].second);
+			t_veciterator it = vec_binary_search(0, i + inserted, pair_vec[i].second);
+			_vec.insert(it, pair_vec[i].second);
 			inserted++;
 		}
 	}
 }
-t_veciterator PmergeMe::vec_binary_search(t_intvec &vec, int l, int r, unsigned int n)
+t_veciterator PmergeMe::vec_binary_search(int l, int r, unsigned int n)
 {
 	while (1)
 	{
 		int size = r - l + 1;
-		if (l >= r)
-			return n < vec[l] ? vec.begin() + l : vec.begin() + l + 1;
+		if (l > r)
+			return _vec.begin() + l;
 		int m = l + size / 2;
-		if (vec[m] == n)
-			return vec.begin() + m;
-		if (n > vec[m])
+		if (_vec[m] == n)
+			return _vec.begin() + m;
+		if (n > _vec[m])
 			l = m + 1;
 		else
 			r = m - 1;
@@ -148,29 +183,33 @@ t_veciterator PmergeMe::vec_binary_search(t_intvec &vec, int l, int r, unsigned 
 	
 	// 1. MergeSort: Splits the input list until there is two elements
 	// 1.1 Creates Pair, sets the larger number to first.
-	///1.2 Reclusive sort
+	///1.2 Recursively sort
 	// 2. Copies larger elements from pair list into sorted list
 	// 3. Insertion
 	// 3.1 Insert at the start of S, the elemented paired with smallest element in S.
 	// 3.2 Insert the remaining, following Jacobsthal sequence.
 	// 4. If the orignal list is odd, insert the last element.
-std::list<unsigned int>  PmergeMe::pmerge_list()
+void  PmergeMe::pmerge_list()
 {
-	std::list<unsigned int> sorted_list;
-
 	if (_list.size() <= 1)
-		return (_list);
+		return ;
 	
-	t_pairlist pair_list = mergeSort(_list.begin(), std::prev(_list.end()));
-	for (t_pair &p : pair_list)
-		sorted_list.push_back(p.first);
-	insertion(sorted_list, pair_list);
+	t_pair odd;
 	if (_list.size() % 2 == 1)
 	{
-		unsigned int insert_num = _list.back();
-		sorted_list.insert(list_binary_search(sorted_list.begin(), sorted_list.end(), insert_num), insert_num);
+		odd = std::make_pair(1, _list.back());
+		_list.pop_back();
 	}
-	return (sorted_list);
+
+	t_pairlist pair_list = mergeSort(_list.begin(), std::prev(_list.end()));
+	
+	_list.clear();
+	for (t_pair &p : pair_list)
+		_list.push_back(p.first);
+
+	insertion(pair_list);
+	if (odd.first)
+		_list.insert(list_binary_search(_list.begin(), _list.end(), odd.second), odd.second);
 }
 
 
@@ -219,9 +258,9 @@ t_pairlist PmergeMe::mergeSort(t_listiterator l, t_listiterator r)
 	return merge(left, right);
 }
 
-void PmergeMe::insertion(t_intlist &list, t_pairlist &pair_list)
+void PmergeMe::insertion(t_pairlist &pair_list)
 {
-	list.push_front(pair_list.front().second);
+	_list.push_front(pair_list.front().second);
 
 	if (pair_list.size() > 1)
 	{
@@ -229,10 +268,10 @@ void PmergeMe::insertion(t_intlist &list, t_pairlist &pair_list)
 		int inserted = 1;
 		for (size_t i = std::min((int) pair_list.size() - 1, 2); i < pair_list.size(); i = get_next_jacobsthal(i, jacob_seq, pair_list.size()))
 		{
-			t_listiterator r = std::next(list.begin(), i + inserted);
+			t_listiterator r = std::next(_list.begin(), i + inserted);
 			int insert_num = std::next(pair_list.begin(), i)->second;
-			t_listiterator it = list_binary_search(list.begin(), r, insert_num);
-			list.insert(it, insert_num);
+			t_listiterator it = list_binary_search(_list.begin(), r, insert_num);
+			_list.insert(it, insert_num);
 			inserted++;
 		}
 	}
@@ -243,14 +282,14 @@ t_listiterator PmergeMe::list_binary_search(t_listiterator l, t_listiterator r, 
 	while (1)
 	{
 		int distance = std::distance(l, r);
-		if (distance < 2)
-			return n < *l ? l : next(l);
+		if (distance <= 1)
+			return n > *l ? std::next(l) : l;
 		std::list<unsigned int>::iterator m = std::next(l, distance / 2);
 		if (*m == n)
 			return (m);
 		if (n > *m)
-			l = std::next(m);
+			l = m;
 		else
-			r = std::prev(m);
+			r = m;
 	}
 }
--- 09/ex02/PmergeMe.hpp
+++ 09/ex02/PmergeMe.hpp
@@ -25,16 +25,17 @@ class PmergeMe
 		std::list<unsigned int>		_list;
 		
 		void fill_containers(int argc, char **argv);
-		std::vector<unsigned int>	pmerge_vec();
-		t_pairvec mergeSort();
-		t_pairvec merge();
-		void insertion(t_pairvec &, t_intvec &);
-		t_veciterator vec_binary_search(t_intvec &vec, int l, int r, unsigned int n);
-		std::list<unsigned int>		pmerge_list();
-		t_pairlist mergeSort(t_listiterator, t_listiterator);
-		t_pairlist merge(t_pairlist &, t_pairlist &);
-		void insertion(t_intlist &, t_pairlist &);
-		t_listiterator list_binary_search(t_listiterator l, t_listiterator r, unsigned int n);
+		void		pmerge_vec();
+		void 		pmerge_list();
+		t_pairvec 	mergeSort(int, int);
+		t_pairlist	mergeSort(t_listiterator, t_listiterator);
+		t_pairvec	merge(t_pairvec, t_pairvec);
+		t_pairlist	merge(t_pairlist &, t_pairlist &);
+		void		insertion(t_pairvec &);
+		void		insertion(t_pairlist &);
+		t_veciterator	vec_binary_search(int l, int r, unsigned int n);
+		t_listiterator	list_binary_search(t_listiterator l, t_listiterator r, unsigned int n);
+		
 		template <typename T>
 		void print(std::string str, T list)
 		{
